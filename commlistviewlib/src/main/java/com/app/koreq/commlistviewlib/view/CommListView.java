3796323130a26beb30c58ad159394796a915c478
KoreQ <mryb1024@gmail.com>
2017-03-22 15:09:16 +0800

    delete xlistview

--- commlistviewlib/src/main/java/com/app/koreq/commlistviewlib/view/CommListView.java
+++ commlistviewlib/src/main/java/com/app/koreq/commlistviewlib/view/CommListView.java
@@ -1,22 +1,32 @@
 package com.app.koreq.commlistviewlib.view;
 
+import android.annotation.TargetApi;
 import android.content.Context;
 import android.util.AttributeSet;
-
-import net.hyww.widget.xlistview.PullToRefreshView;
+import android.widget.ListView;
 
 /**
  * project : CommListView
  * author : YanBin on 3/16/17
  * description : a custom common PullToRefreshListView
  */
-public class CommListView extends PullToRefreshView{
+public class CommListView extends ListView {
+
+
+    public CommListView(Context context) {
+        super(context);
+    }
 
     public CommListView(Context context, AttributeSet attrs) {
         super(context, attrs);
     }
 
-    public CommListView(Context context) {
-        super(context);
+    public CommListView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    @TargetApi(21)
+    public CommListView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
     }
 }
