79bd20a564647ba57014656708e3eba40c02caf1
KoreQ <mryb1024@gmail.com>
2017-03-22 11:10:06 +0800

    add comment and test data

--- commlistviewlib/src/main/java/com/app/koreq/commlistviewlib/adapter/BaseListAdapter.java
+++ commlistviewlib/src/main/java/com/app/koreq/commlistviewlib/adapter/BaseListAdapter.java
@@ -13,25 +13,36 @@ import com.app.koreq.commlistviewlib.render.BaseListRender;
  */
 public abstract class BaseListAdapter extends BaseAdapter {
 
+    /**
+     * @return int 即getViewTypeCount 最小为1
+     */
     public abstract int getCustomViewTypeCount();
 
+    /**
+     * @return int getItemViewType
+     */
     public abstract int getCustomItemViewType(int position);
 
+    /**
+     * 返回当前item对应的渲染类
+     *
+     * @return BaseListRender 根据类型返回对应的渲染类
+     */
     public abstract BaseListRender getCustomListRender(int position);
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         BaseListRender render = null;
-        if(convertView == null){
+        if (convertView == null) {
             render = getCustomListRender(position);
             convertView = render.getContentView();
             convertView.setTag(render);
 
-        }else{
+        } else {
             render = (BaseListRender) convertView.getTag();
         }
 
-        if(render != null){
+        if (render != null) {
             render.fillDataToView(position);
         }
 
