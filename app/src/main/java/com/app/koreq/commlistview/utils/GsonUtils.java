91261928b2eb55d5d28760358c8b30c4d83712a5
koreQ <mryb1024@gmail.com>
2017-04-12 14:29:05 +0800

    add AESUtils HexUtils and modify GsonUtils

--- app/src/main/java/com/app/koreq/commlistview/utils/GsonUtils.java
+++ app/src/main/java/com/app/koreq/commlistview/utils/GsonUtils.java
@@ -4,8 +4,6 @@ import com.app.koreq.commlistview.gson.CustomDeserializer;
 import com.app.koreq.commlistviewlib.bean.BaseListBean;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonParser;
 
 /**
  * project : CommListView
@@ -17,14 +15,14 @@ public class GsonUtils {
     private static Gson gsonInstance;
     private static Gson gsonCustomInstance;
 
-    public static Gson getGson() {
+    public synchronized static Gson getGson() {
         if (gsonInstance == null) {
             gsonInstance = new Gson();
         }
         return gsonInstance;
     }
 
-    public static Gson getCustomGson() {
+    public synchronized static Gson getCustomGson() {
         if (gsonCustomInstance == null) {
             GsonBuilder builder = new GsonBuilder();
             builder.registerTypeAdapter(BaseListBean.class, new CustomDeserializer());
