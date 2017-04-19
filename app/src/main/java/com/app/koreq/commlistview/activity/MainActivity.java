309c7417ccdec84c3e3581a2ac5e9f69c7a009ee
koreQ <mryb1024@gmail.com>
2017-04-19 16:02:15 +0800

    MainActivity - 增加入口、增加资源属性测试

--- app/src/main/java/com/app/koreq/commlistview/activity/MainActivity.java
+++ app/src/main/java/com/app/koreq/commlistview/activity/MainActivity.java
@@ -5,9 +5,9 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 import com.app.koreq.commlistview.R;
-import com.app.koreq.commlistview.activity.CommListActivity;
 import com.app.koreq.commlistview.activity.widget.PairScrollAct;
 
 /**
@@ -31,10 +31,14 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         Button btnCommList = (Button) findViewById(R.id.btn_comm_list);
         Button btnHttpTest = (Button) findViewById(R.id.btn_http_test);
         Button btnPairTest = (Button) findViewById(R.id.btn_pairview_test);
+        Button btnRxjavaTest = (Button) findViewById(R.id.btn_rxjava_test);
+        Button btnGradleStr = (Button) findViewById(R.id.btn_gradle_str_test);
 
         btnCommList.setOnClickListener(this);
         btnHttpTest.setOnClickListener(this);
         btnPairTest.setOnClickListener(this);
+        btnRxjavaTest.setOnClickListener(this);
+        btnGradleStr.setOnClickListener(this);
     }
 
     private void initData() {
@@ -51,10 +55,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btn_http_test:    //网络请求
                 startOtherActivity(HttpTestAct.class);
                 break;
-            case R.id.btn_pairview_test:    //PairView请求
+            case R.id.btn_pairview_test:    //PairView
                 PairScrollAct.launchAct(this);
                 break;
+            case R.id.btn_rxjava_test:    //Rxjava
+                RxJavaActivity.launchAct(this);
+                break;
+            case R.id.btn_gradle_str_test:    //Rxjava
+                toastGradleRes();
+                break;
         }
+    }
+
+    /**
+     * build.gradle文件中定义的资源属性 测试方法
+     */
+    private void toastGradleRes() {
+//        String hostEnv = getResources().getString(R.string.host_env);   //正常
+//        String currentTime = getResources().getString(R.string.current_time);   //正常
+//        Toast.makeText(this, "hostEnv = " + hostEnv + ", " +
+//                "currentTime = " + currentTime, Toast.LENGTH_LONG).show();    //正常
+
+//        boolean IS_DEBUG = getResources().getBoolean(R.bool.IS_DEBUG);   //正常
+//        int testNum1 = getResources().getInteger(R.integer.test_num_1);   //正常
+//        int testNum2 = getResources().getInteger(R.integer.test_num_2);   //正常
+//        Toast.makeText(this, "IS_DEBUG = " + IS_DEBUG + ", " +
+//                "testNum1 = " + testNum1 + ", testNum2 = " + testNum2, Toast.LENGTH_LONG).show();    //正常
+
+//        String testStr = getResources().getString(R.string.test_string);   //正常
+//        int testInt = getResources().getInteger(R.integer.test_int);   //正常
+//        boolean testBool = getResources().getBoolean(R.bool.test_bool);   //正常
+//        Toast.makeText(this, "、" + testStr + "、" + testInt + "、" + testBool, Toast.LENGTH_LONG).show();    //正常
+
+        int hostEnv = getResources().getInteger(R.integer.host_env);
+        boolean isDebug = getResources().getBoolean(R.bool.is_debug);
+        Toast.makeText(this, hostEnv + "、" + isDebug, Toast.LENGTH_LONG).show();    //正常
     }
 
     private void startOtherActivity(Class<?> cls) {
