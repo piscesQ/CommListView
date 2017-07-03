43d7b2052b31b9386bc180fc1efb26840fec9746
koreQ <mryb1024@gmail.com>
2017-07-03 15:03:11 +0800

    网络状态管理工具

---
+++ commlib/src/main/java/com/app/koreq/commlib/utils/NetManager.java
@@ -0,0 +1,155 @@
+package com.app.koreq.commlib.utils;
+
+import android.content.Context;
+import android.location.LocationManager;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.telephony.TelephonyManager;
+
+/**
+ * project : CommListView
+ * author : YanBin on 7/3/17
+ * version : v
+ * description : 网络状态 管理工具类：需要权限 ACCESS_NETWORK_STATE
+ */
+public class NetManager {
+
+    /**
+     * 判断是否有网络连接
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isNetworkConnected(Context context) {
+        if (context != null) {
+            // 获取手机所有连接管理对象(包括对wi-fi,net等连接的管理)
+            ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            // 获取NetworkInfo对象
+            NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+            //判断NetworkInfo对象是否为空
+            if (networkInfo != null)
+                return networkInfo.isAvailable();
+        }
+        return false;
+    }
+
+    /**
+     * 判断WIFI网络是否可用
+     *
+     * @param context
+     * @param context
+     * @return
+     */
+    public static boolean isWifiConnected(Context context) {
+        if (context != null) {
+            // 获取手机所有连接管理对象(包括对wi-fi,net等连接的管理)
+            ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            // 获取NetworkInfo对象
+            NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+            //判断NetworkInfo对象是否为空 并且类型是否为WIFI
+            if (networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_WIFI)
+                return networkInfo.isAvailable();
+        }
+        return false;
+    }
+
+    /**
+     * 判断MOBILE网络是否可用
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isMobileConnected(Context context) {
+        if (context != null) {
+            //获取手机所有连接管理对象(包括对wi-fi,net等连接的管理)
+            ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            //获取NetworkInfo对象
+            NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+            //判断NetworkInfo对象是否为空 并且类型是否为MOBILE
+            if (networkInfo != null && networkInfo.getType() == ConnectivityManager.TYPE_MOBILE)
+                return networkInfo.isAvailable();
+        }
+        return false;
+    }
+
+    /**
+     * 获取当前网络连接的类型信息
+     * 原生
+     *
+     * @param context
+     * @return
+     */
+    public static int getConnectedType(Context context) {
+        if (context != null) {
+            //获取手机所有连接管理对象
+            ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+            //获取NetworkInfo对象
+            NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isAvailable()) {
+                //返回NetworkInfo的类型
+                return networkInfo.getType();
+            }
+        }
+        return -1;
+    }
+
+    /**
+     * 获取当前的网络状态 ：没有网络=0；WIFI网络=1；2G网络=2；3G网络=3；4G网络=4；
+     * 自定义
+     *
+     * @param context
+     * @return
+     */
+    public static int getCustomNetType(Context context) {
+        //结果返回值
+        int netType = 0;
+        //获取手机所有连接管理对象
+        ConnectivityManager manager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        //获取NetworkInfo对象
+        NetworkInfo networkInfo = manager.getActiveNetworkInfo();
+        //NetworkInfo对象为空 则代表没有网络
+        if (networkInfo == null) {
+            return netType;
+        }
+        //否则 NetworkInfo对象不为空 则获取该networkInfo的类型
+        int nType = networkInfo.getType();
+        if (nType == ConnectivityManager.TYPE_WIFI) {
+            //WIFI
+            netType = 1;
+        } else if (nType == ConnectivityManager.TYPE_MOBILE) {
+            int nSubType = networkInfo.getSubtype();
+            TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(Context.TELEPHONY_SERVICE);
+
+            if (nSubType == TelephonyManager.NETWORK_TYPE_LTE           //4G
+                    && !telephonyManager.isNetworkRoaming()) {
+                netType = 4;
+            } else if (nSubType == TelephonyManager.NETWORK_TYPE_UMTS   //(3G)移动、联通
+                    || nSubType == TelephonyManager.NETWORK_TYPE_HSDPA  //(3G)联通的3G为HSDPA或HSDPAP
+                    || nSubType == TelephonyManager.NETWORK_TYPE_EVDO_0 //(3G)电信的3G为EVDO
+                    && !telephonyManager.isNetworkRoaming()) {
+                netType = 3;
+            } else if (nSubType == TelephonyManager.NETWORK_TYPE_GPRS   //(2.5G)  移动和联通
+                    || nSubType == TelephonyManager.NETWORK_TYPE_EDGE   //(2.75G) 2.5G到3G的过渡  移动和联通
+                    || nSubType == TelephonyManager.NETWORK_TYPE_CDMA   //(2G)    电信
+                    && !telephonyManager.isNetworkRoaming()) {
+                netType = 2;
+            } else {
+                netType = 2;
+            }
+        }
+        return netType;
+    }
+
+    /**
+     * 判断GPS是否打开
+     * ACCESS_FINE_LOCATION权限
+     *
+     * @param context
+     * @return
+     */
+    public static boolean isGPSEnabled(Context context) {
+        //获取手机所有连接LOCATION_SERVICE对象
+        LocationManager locationManager = ((LocationManager) context.getSystemService(Context.LOCATION_SERVICE));
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER);
+    }
+}
